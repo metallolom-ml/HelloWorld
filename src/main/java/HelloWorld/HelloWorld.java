7177e8e7ab04932d30213b1c5e2d6bc9a6bfd814
vitalykolesov <metalx@list.ru>
2023-07-16 22:43:14 +0300

    ДЗ выполнено

--- src/main/java/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Включить программу");
         System.out.println("Hello World!");
+        System.out.println("Всем спасибо, все свободны");
         System.out.println("Выключить программу");
     }
 }
