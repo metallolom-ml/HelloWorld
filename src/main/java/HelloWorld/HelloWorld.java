1e89f7a6665fad14bb50c84de0f24d9898b5a304
vitalykolesov <metalx@list.ru>
2023-07-16 22:23:43 +0300

    init

---
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Включить программу");
+        System.out.println("Hello World!");
+        System.out.println("Выключить программу");
+    }
+}
